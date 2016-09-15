846c0f4a7b8715ab32a14e95da288c68878cae4e
Alejandro Guerrero <alex13gro@gmail.com>
2016-09-15 09:55:26 -0500

    butterknife changes

--- MyFirstApp/app/src/main/java/com/a13x/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/a13x/myfirstapp/MainActivity.java
@@ -1,33 +1,31 @@
 package com.a13x.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSend;
+
+    @Bind(R.id.edit_message)
+    EditText message;
+    @Bind(R.id.buttonSend)
+    Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        btnSend = findViewById(R.id.buttonSend);
-
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        })
-
-    public void sendMessage(View view) {
-
-        Log.v("MENSAJE DE LOG", "MENSAJE NO DISTRAEñR");
     }
 
     public void sendMessage(View view) {
@@ -38,13 +36,15 @@ public class MainActivity extends AppCompatActivity {
         Log.v("Escribi:", message.getText().ToString());
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    @OnClick({R.id.edit_message, R.id.buttonSend})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edit_message:
+                break;
+            case R.id.buttonSend:
+                break;
+        }
     }
 }
 }
+        }
