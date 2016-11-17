216c34034e50c7658b8b81291072d7747458b0ba
Alejandro Guerrero <alex13gro@gmail.com>
2016-11-17 09:31:05 -0600

    Add Layout

---
+++ DicisTest/app/src/main/java/com/a13x/dicistest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.a13x.dicistest;
+
+/**
+ * Created by Alejandro on 17/11/2016.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
