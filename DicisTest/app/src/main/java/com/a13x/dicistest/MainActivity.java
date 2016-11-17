deab19d49991bb93ddfeac0916f03a776f461354
Alejandro Guerrero <alex13gro@gmail.com>
2016-11-17 09:35:55 -0600

    Add Butteknife scaffold

--- DicisTest/app/src/main/java/com/a13x/dicistest/MainActivity.java
+++ DicisTest/app/src/main/java/com/a13x/dicistest/MainActivity.java
@@ -3,14 +3,32 @@ package com.a13x.dicistest;
 /**
  * Created by Alejandro on 17/11/2016.
  */
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.editText)
+    EditText editText;
+    @Bind(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void clickSubmitHandler() {
+        Log.v("HOLA", "HOLA");
     }
 }
